9d0aac811ad6634ae8088a3b96bfd9ced4cb172e
Brady Stephenson <brmstephenson@gmail.com>
2017-08-27 15:19:58 -0400

    Add more style by adding sunglasses

--- src/main/js/app.jsx
+++ src/main/js/app.jsx
@@ -15,7 +15,21 @@ class App extends Component {
         super(props);
         this.generate = this.generate.bind(this);
         this.state = {
-            tweet: 'Generate a Tweet By Clicking Below!'
+            tweet: 'Generate a Tweet By Clicking Below!',
+            avatars: [
+                {
+                    profile: 'https://twitter.com/lesleyrosem',
+                    image: 'https://pbs.twimg.com/profile_images/892493508380053504/ima7_lsJ_normal.jpg'
+                },
+                {
+                    profile: 'https://twitter.com/tomslyck',
+                    image: 'https://pbs.twimg.com/profile_images/3062777262/675a207de759ab3efd2fa5dd6ad40fc1_normal.jpeg'
+                },
+                {
+                    profile: 'https://twitter.com/DebbieJ73420522',
+                    image: 'https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png',
+                }
+            ]
         }
     }
 
@@ -36,6 +50,23 @@ class App extends Component {
             });
     }
 
+    createAvatarLi() {
+
+        let avatarElements = this.state.avatars.map(avatar => this.createAvatar(avatar));
+
+        return (
+            <li className="tweet-avatar-row">
+                {avatarElements}
+            </li>
+        )
+    }
+
+    createAvatar(avatar) {
+        return (
+            <a className="tweet-avatar" href={avatar.profile}><img src={avatar.image}/></a>
+        )
+    }
+
 
     render() {
         const self = this;
@@ -49,7 +80,10 @@ class App extends Component {
                                 <img src="https://pbs.twimg.com/profile_images/874276197357596672/kUuht00m_bigger.jpg"/>
                             </div>
                             <div className="tweet-account-container">
-                                <h4 className="tweet-name"><a href="#">Donald J. Trump</a></h4>
+                                <h4 className="tweet-name">
+                                    <a className="tweet-blue-link" href="#">Donald J. Trump</a>
+                                    <i className="tweet-verified edge-icon edge-icon-verified" />
+                                </h4>
                                 <h6 className="tweet-username">@realDonaldTrump</h6>
                             </div>
                             <div className="tweet-follow-button">
@@ -61,6 +95,31 @@ class App extends Component {
                         {this.state.tweet}
                     </p>
                     <div className="tweet-timestamp">3:46 PM - 17 Aug 2017</div>
+                    <ul className="tweet-stats unstyled-list">
+                        <li className="tweet-stat tweet-retweets"><a href="#"><strong>19,253</strong>Retweets</a></li>
+                        <li className="tweet-stat tweet-likes"><strong>73,910</strong>Likes</li>
+                       {this.createAvatarLi()}
+                    </ul>
+                    <ul className="tweet-action-list unstyled-list">
+                        <li className="tweet-reply">
+                            <a className="tweet-gray tweet-blue-link" href="#">
+                                <i className="edge-icon edge-icon-reply" />
+                                <span>9.9K</span>
+                            </a>
+                        </li>
+                        <li className="tweet-retweet">
+                            <a className="tweet-gray tweet-green-link" href="#">
+                                <i className="edge-icon edge-icon-retweet" />
+                                <span>148</span>
+                            </a>
+                        </li>
+                        <li className="tweet-heart">
+                            <a className="tweet-gray tweet-red-link" href="#">
+                                <i className="edge-icon edge-icon-heart" />
+                                <span>39K</span>
+                            </a>
+                        </li>
+                    </ul>
                 </div>
             </div>
             <div>
